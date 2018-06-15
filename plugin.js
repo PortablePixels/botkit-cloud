fb8217e6a8ef35f14e31eca928c7232f9c32a4e1
Ben Brown <ben@xoxco.com>
2018-06-15 10:43:01 -0500

    report stats back to studio

--- plugin.js
+++ plugin.js
@@ -81,8 +81,8 @@ module.exports = function(botkit) {
                     botkit.middleware.onChange.use(function(convo, key, val, next) {
                         if (convo.script.command == script_name && key == var_name) {
                             try {
-                                handler(convo, function() {
-                                    next();
+                                handler(convo, function(err) {
+                                    next(err);
                                 });
                             } catch (err) {
                                 next(err);
@@ -111,6 +111,25 @@ module.exports = function(botkit) {
 
         },
         middleware: {
+            afterScript: function(convo, next) {
+              if (!botkit.config.stats_optout) {
+                var data = {
+                    user: md5(convo.context.user),
+                    channel: md5(convo.context.channel),
+                    command: convo.script.command,
+                    conversation_length: convo.lastActive - convo.startTime,
+                    status: convo.status,
+                    type: 'remote_command_end',
+                    final_thread: convo.state.thread,
+                    bot_type: convo.bot.type,
+                };
+                statsAPI(bot, {
+                    method: 'post',
+                    form: data,
+                }, message);
+                next();
+              }
+            },
             understand: [
                 function(bot, message, response, next) {
                     // pass it through the ol' Botkit Studio trigger API
@@ -142,4 +161,123 @@ module.exports = function(botkit) {
             ]
         }
     }
+
+
+
+
+        function statsAPI(bot, options, message) {
+            var _STUDIO_STATS_API = controller.config.studio_stats_uri || 'https://stats.botkit.ai';
+            options.uri = _STUDIO_STATS_API + '/api/v1/stats';
+
+            return new Promise(function(resolve, reject) {
+
+                var headers = {
+                    'content-type': 'application/json',
+                };
+
+                if (bot.config && bot.config.studio_token) {
+                    options.uri = options.uri + '?access_token=' + bot.config.studio_token;
+                } else if (botkit.config && botkit.config.studio_token) {
+                    options.uri = options.uri + '?access_token=' + botkit.config.studio_token;
+                } else {
+                    // do nothing - making an unathenticated request to the stats api...
+                }
+
+                options.headers = headers;
+
+                var stats_body = {};
+                stats_body.botHash = botHash(bot);
+                if (bot.type == 'slack' && bot.team_info) {
+                    stats_body.team = md5(bot.team_info.id);
+                }
+
+                if (bot.type == 'ciscospark' && message && message.raw_message && message.raw_message.orgId) {
+                    stats_body.team = md5(message.raw_message.orgId);
+                }
+
+                if (bot.type == 'teams' && bot.config.team) {
+                    stats_body.team = md5(bot.config.team);
+                }
+
+                stats_body.channel = options.form.channel;
+                stats_body.user = options.form.user;
+                stats_body.type = options.form.type;
+                stats_body.time = now;
+                stats_body.meta = {};
+                stats_body.meta.user = options.form.user;
+                stats_body.meta.channel = options.form.channel;
+                if (options.form.final_thread) {
+                    stats_body.meta.final_thread = options.form.final_thread;
+                }
+                stats_body.meta.conversation_length = options.form.conversation_length;
+                stats_body.meta.status = options.form.status;
+                stats_body.meta.type = options.form.type;
+                stats_body.meta.command = options.form.command;
+                options.form = stats_body;
+                request(options, function(err, res, body) {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    var json = null;
+
+                    try {
+                        json = JSON.parse(body);
+                    } catch (e) {}
+
+                    if (!json || json == null) {
+                        return reject('Response from Botkit Studio API was empty or invalid JSON');
+                    } else if (json.error) {
+                        if (res.statusCode === 401) {
+                            console.error(json.error);
+                        }
+                        return reject(json.error);
+                    } else {
+                        resolve(json);
+                    }
+                });
+            });
+        }
+
+        /* generate an anonymous hash to uniquely identify this bot instance */
+        function botHash(bot) {
+            var x = '';
+            switch (bot.type) {
+                case 'slack':
+                    if (bot.config.token) {
+                        x = md5(bot.config.token);
+                    } else {
+                        x = 'non-rtm-bot';
+                    }
+                break;
+                case 'teams':
+                    x = md5(bot.identity.id);
+                break;
+                case 'fb':
+                    x = md5(bot.botkit.config.access_token);
+                break;
+                case 'twilioipm':
+                    x = md5(bot.config.TWILIO_IPM_SERVICE_SID);
+                break;
+                case 'twiliosms':
+                    x = md5(bot.botkit.config.account_sid);
+                break;
+                case 'ciscospark':
+                    x = md5(bot.botkit.config.ciscospark_access_token);
+                break;
+                default:
+                    x = 'unknown-bot-type';
+                break;
+            }
+            return x;
+        };
+
+
+        /* Every time a bot spawns, Botkit calls home to identify this unique bot
+         * so that the maintainers of Botkit can measure the size of the installed
+         * userbase of Botkit-powered bots. */
+
+
+
+
 }
